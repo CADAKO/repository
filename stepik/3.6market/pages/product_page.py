7833d7652f3a65137139b364b27348b62b2dce79
Denis <cadako@gmail.com>
2025-10-29 17:24:22 +0300

    Create product_page.py

---
+++ stepik/3.6market/pages/product_page.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class ProductPage:
+    ADD_TO_BSKT_BTN = (By.CLASS_NAME, "btn-add-to-basket")
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self, url):
+        self.driver.get(url)
+
+    def find_button(self):
+        try:
+            wait = WebDriverWait(self.driver, 5)
+            wait.until(EC.presence_of_all_elements_located(self.ADD_TO_BSKT_BTN))
+            buttons = self.driver.find_elements(*self.ADD_TO_BSKT_BTN)
+            return len(buttons)
+        except:
+            return 0
